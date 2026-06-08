f95a0c9e2e90c9d99fe381c8f36760e7421bfb7e
Ritik <Ritikkushwaha1234@gmail.com>
2026-06-08 18:08:41 +0530

    LIB-10: Haptic feedback on number pad; reuse keypad on /entries/new

--- src/components/ledger/CustomerLedgerClient.tsx
+++ src/components/ledger/CustomerLedgerClient.tsx
@@ -14,6 +14,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { CURRENCIES } from "@/lib/currency";
 import { toMinor, formatMoney } from "@/lib/money";
+import { cn } from "@/lib/utils";
 
 type Entry = {
   id: string;
